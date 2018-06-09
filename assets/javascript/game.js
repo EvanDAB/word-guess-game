89eef11543ae46fd22f80fa70db60b9672168e0c
Evan Carr <37350180+EvanDAB@users.noreply.github.com>
2018-06-09 13:20:44 -0700

    now the letters fill out the space but you need to add win and lose statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,43 +6,43 @@ var rightGuess = sitcoms[randomWord];
 var rGuess = rightGuess.toLowerCase();
 var guessRemaining = 20;
 console.log(guessRemaining);
-
 console.log(rightGuess);
 
-// this is the basic format for how the game will work
+var lettersGuessed = [];
+
+var answerArray = [];
+for (var i = 0; i < rGuess.length; i++) {
+answerArray[i] = "_";
+}
+
 function playerGuess (input) {
     var key = event.key;
     var remainingLetters = rGuess.length;
     var letterList= document.querySelector('#lettersGuessed');
-    letterList.innerText = letterList.innerText + ` ${key}, `;
+    letterList.innerText += ` ${key}, `;
     document.querySelector("#guessR").innerText = guessRemaining;
 
-    var lettersGuessed = [];
-
-    var answerArray = [];
-    for (var i = 0; i < rGuess.length; i++) {
-    answerArray[i] = "_";
-    }
+    
     document.querySelector("#rGuess").innerText = answerArray;
     
-    if (rGuess.indexOf(key) != -1) { // if the character is found
-        for (var i = 0; i < rightGuess.length; i ++) { // loop on all characters
+    if (rGuess.indexOf(key) != -1) { 
+        for (var i = 0; i < rightGuess.length; i ++) { 
             if (rGuess[i] == key) { 
             guessRemaining--;
-            // hangman fills out but it does not keep going
-            document.querySelector("#rGuess").innerText = answerArray.join(``);
+            var x = answerArray.join("");
+            document.querySelector("#rGuess").innerText = x;
+            console.log("array =>", answerArray)
             answerArray[i] = key;
+
             } 
         }
+        document.querySelector("#rGuess").innerText = answerArray.join(" ");
     } else { 
-            //alert('naw');
-            // all of my 'no' conditions should be here
             guessRemaining--;
 
     }   
 
     if (guessRemaining == 0) {
-        //loss
         guessRemaining = 20;
         rightGuess = sitcoms[Math.floor(Math.random()*sitcoms.length)];
         letterList.innerText =  ` `;
